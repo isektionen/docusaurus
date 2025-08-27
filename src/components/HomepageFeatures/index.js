3c7d79e29942f0f4942ba036a86339c8f8808111
Erik Sandlöv <erik@sandlov.com>
2025-08-27 11:11:23 +0200

    added some spacing between the stats and sponsors

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -29,8 +29,8 @@ const FeatureList = [
     description: (
       <>
         <Translate>Vill du engagera dig i sektionen? Kolla in våra</Translate><Link
-            to={process.env.DOCUSAURUS_CURRENT_LOCALE === 'sv' ? '/docs/intro' : '/en/docs/intro'}>
-        <Translate>nämnder</Translate>
+          to={process.env.DOCUSAURUS_CURRENT_LOCALE === 'sv' ? '/docs/intro' : '/en/docs/intro'}>
+          <Translate>nämnder</Translate>
         </Link><Translate>och hitta en som passar dig!</Translate>
       </>
     ),
@@ -39,15 +39,15 @@ const FeatureList = [
     title: <Translate>Välmående</Translate>,
     img: require('@site/static/img/pride.jpg').default,
     description: (
-    
-    
-    <>
-    <Translate>Vi vill att alla ska må bra. Läs mer om hur vi jobbar med</Translate><Link
-        to={process.env.DOCUSAURUS_CURRENT_LOCALE === 'sv' ? '/trygghet' : '/en/trygghet'}>
-      <Translate>välmående</Translate>
-    </Link><Translate> på sektionen.</Translate>
-    </>
-      
+
+
+      <>
+        <Translate>Vi vill att alla ska må bra. Läs mer om hur vi jobbar med</Translate><Link
+          to={process.env.DOCUSAURUS_CURRENT_LOCALE === 'sv' ? '/trygghet' : '/en/trygghet'}>
+          <Translate>välmående</Translate>
+        </Link><Translate> på sektionen.</Translate>
+      </>
+
     ),
   },
   {
@@ -55,16 +55,16 @@ const FeatureList = [
     img: require('@site/static/img/dinner.jpg').default,
 
     description: (
-    
-    <>
-      <Translate>Vill du samarbeta med oss? Läs mer om hur vi jobbar med</Translate>
-     <Link
+
+      <>
+        <Translate>Vill du samarbeta med oss? Läs mer om hur vi jobbar med</Translate>
+        <Link
           to={process.env.DOCUSAURUS_CURRENT_LOCALE === 'sv' ? '/business' : '/en/business'}>
-        <Translate>företag</Translate>
-      </Link><Translate> på sektionen.</Translate>
-    </>
-      
-      
+          <Translate>företag</Translate>
+        </Link><Translate> på sektionen.</Translate>
+      </>
+
+
     ),
   },
 ];
@@ -74,7 +74,7 @@ function Feature({ img, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img role="img" src={img} alt={""}/>
+        <img role="img" src={img} alt={""} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -87,13 +87,13 @@ function Feature({ img, title, description }) {
 
 
 export default function HomepageFeatures() {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   return (
     <section className={styles.features}>
       <div className="container">
         <h2>
           <Translate>
-          Kommande evenemang
+            Kommande evenemang
           </Translate>
         </h2>
         <EventCalendar />
@@ -110,98 +110,103 @@ export default function HomepageFeatures() {
 
         </div>
 
-        <div className="text--center">
+        {/* Stats Section */}
+        <div className="text--center" style={{ marginTop: '4rem', marginBottom: '4rem', padding: '2rem 0', borderTop: '1px solid var(--ifm-color-emphasis-300)', borderBottom: '1px solid var(--ifm-color-emphasis-300)' }}>
           <h2>
             <Translate>
-                Sektionen i siffror
+              Sektionen i siffror
             </Translate>
           </h2>
           <Stats />
         </div>
+
+        {/* Sponsors Section */}
+        <div style={{ marginTop: '4rem' }}>
           <div className="text--center">
-              <h2>
-                  <Translate>
-                      Våra Samarbetspartners
-                  </Translate>
-              </h2>
+            <h2>
+              <Translate>
+                Våra Samarbetspartners
+              </Translate>
+            </h2>
           </div>
           <div className="row" style={{ justifyContent: "center", textAlign: "center" }}>
-              <div className="col col--3">
-                  <a href={"https://appliedvaluegroup.com/"}>
-                      {colorMode === 'dark' ? (
-                          <AvgIconDark width={200} height={100}/>
-                      ) : (
-                          <AvgIcon width={200} height={100}/>
-                      )}
-                  </a>
-              </div>
+            <div className="col col--3">
+              <a href={"https://appliedvaluegroup.com/"}>
+                {colorMode === 'dark' ? (
+                  <AvgIconDark width={200} height={100} />
+                ) : (
+                  <AvgIcon width={200} height={100} />
+                )}
+              </a>
+            </div>
 
-              
-              <div className="col col--3">
-                  <a href={"https://www.bain.com/"}>
-                      {colorMode === 'dark' ? (
-                          <BainIconDark width={200} height={100} />
-                      ) : (
-                          <BainIcon width={200} height={100} />
-                      )}
-                  </a>
-              </div>
 
-              <div className="col col--3">
-                    <a href={"https://www2.deloitte.com/se/sv.html"}>
-                        {colorMode === 'dark' ? (
-                            <DeloitteIconDark width={200} height={100}/>
-                        ) : (
-                            <DeloitteIcon width={200} height={100}/>
-                        )}
-                        </a>
-              </div>
+            <div className="col col--3">
+              <a href={"https://www.bain.com/"}>
+                {colorMode === 'dark' ? (
+                  <BainIconDark width={200} height={100} />
+                ) : (
+                  <BainIcon width={200} height={100} />
+                )}
+              </a>
+            </div>
+
+            <div className="col col--3">
+              <a href={"https://www2.deloitte.com/se/sv.html"}>
+                {colorMode === 'dark' ? (
+                  <DeloitteIconDark width={200} height={100} />
+                ) : (
+                  <DeloitteIcon width={200} height={100} />
+                )}
+              </a>
+            </div>
           </div>
           <div className="row" style={{ justifyContent: "center", textAlign: "center" }}>
-              <div className="col col--3">
-                  <a href={"https://www.mckinsey.com/careers/"}>
-                      {colorMode === 'dark' ? (
-                          <MckinseyIconDark width={200} height={100} />
-                      ) : (
-                          <McKinseyIcon width={200} height={100} />
-                      )}
-                  </a>
-              </div>
+            <div className="col col--3">
+              <a href={"https://www.mckinsey.com/careers/"}>
+                {colorMode === 'dark' ? (
+                  <MckinseyIconDark width={200} height={100} />
+                ) : (
+                  <McKinseyIcon width={200} height={100} />
+                )}
+              </a>
+            </div>
 
-              <div className="col col--3">
-                    <a href={"https://www.rolandberger.com/en/"}>
-                        {colorMode === 'dark' ? (
-                            <RolandBergerIconDark width={200} height={100}/>
-                        ) : (
-                            <RolandBergerIcon width={200} height={100}/>
-                        )}
-                    </a>
-              </div>
+            <div className="col col--3">
+              <a href={"https://www.rolandberger.com/en/"}>
+                {colorMode === 'dark' ? (
+                  <RolandBergerIconDark width={200} height={100} />
+                ) : (
+                  <RolandBergerIcon width={200} height={100} />
+                )}
+              </a>
+            </div>
 
-              <div className="col col--3">
-                  <a href={"https://www.pwc.se/karriar/"}>
-                      {colorMode === 'dark' ? (
-                          <PwcIconDark width={200} height={100} />
-                      ) : (
-                          <PwcIcon width={200} height={100} />
-                      )}
-                  </a>
-              </div>
+            <div className="col col--3">
+              <a href={"https://www.pwc.se/karriar/"}>
+                {colorMode === 'dark' ? (
+                  <PwcIconDark width={200} height={100} />
+                ) : (
+                  <PwcIcon width={200} height={100} />
+                )}
+              </a>
+            </div>
 
-              <div className="col col--3">
-                  <a href={"https://www.bcg.com/"}>
-                      {colorMode === 'dark' ? (
-                          <BcgIconDark width={150} height={100}/>
-                      ) : (
-                          <BcgIcon width={150} height={100}/>
-                      )}
-                  </a>
-              </div>
+            <div className="col col--3">
+              <a href={"https://www.bcg.com/"}>
+                {colorMode === 'dark' ? (
+                  <BcgIconDark width={150} height={100} />
+                ) : (
+                  <BcgIcon width={150} height={100} />
+                )}
+              </a>
+            </div>
           </div>
+        </div>
       </div>
 
     </section>
   );
-  
+
 }
 
